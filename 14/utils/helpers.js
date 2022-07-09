b010112daee12ce1496d2dcf7c19fc5e6f41c8d3
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-09 10:48:10 +0100

    ex14 done

--- 14/utils/helpers.js
+++ 14/utils/helpers.js
@@ -4,10 +4,4 @@ module.exports = {
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
   // adds five years to the date, and formats it as M/D/YYYY
-  format_date: (date) => {
-    var year = date.getFullYear() + 5;
-    var month = date.getMonth();
-    var day = date.getDate();
-    return `${day}/${month}/${year}`;
-  },
 };
